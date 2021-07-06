338eb25b4d2d0258fa5db55a530541cc71d84936
lazo92 <jlazo912@gmail.com>
2021-07-06 11:53:04 -0300

    cambio 2

--- 1-EntradaSalida/jsEntradaSalida-01.js
+++ 1-EntradaSalida/jsEntradaSalida-01.js
@@ -1,8 +1,8 @@
 /*
 Al presionar el  botón, 
-se debe mostrar un mensaje como el siguiente "Esto funciona de maravilla"*/
+se debe mostrar un mensaje como el siguiente "Esto funciona de maravila"*/
 function mostrar()
 {
-	alert("Hola 2021");
+	alert("Esto funciona de maravila");
 }
 
